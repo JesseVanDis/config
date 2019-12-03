1f3ecc39bfec70081cda4da35db6fdda72a1477c
Jesse van Dis <jessevandis@gmail.com>
2019-12-03 11:46:03 +0100

    Update install_emacs.sh

--- install_emacs.sh
+++ install_emacs.sh
@@ -2,6 +2,7 @@ cd ~/.emacs.d/emacs26
 wget http://mirror.netcologne.de/gnu/emacs/emacs-26.1.tar.xz
 tar xvfJ ./emacs-26.1.tar.xz
 cd ./emacs-26.1/
+!dpkg -s libgtk-3-dev 2>/dev/null >/dev/null || sudo apt-get install libgtk-3-dev
 !dpkg -s libjpeg-dev 2>/dev/null >/dev/null || sudo apt-get install libjpeg-dev
 !dpkg -s libtiff-dev 2>/dev/null >/dev/null || sudo apt-get install libtiff-dev
 !dpkg -s libgif-dev 2>/dev/null >/dev/null || sudo apt-get install libgif-dev
