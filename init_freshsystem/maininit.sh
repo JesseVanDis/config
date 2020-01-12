eb36b50ae84996bdbc163938126009384a42b1a8
Jesse <jesse@example.com>
2020-01-12 16:10:08 +0100

    added questions for all instal options

--- init_freshsystem/maininit.sh
+++ init_freshsystem/maininit.sh
@@ -16,44 +16,64 @@ if [ "$readPreventStandby" == "y" ]; then
 fi
 
 
-#graphic driver
-echo ":: graphics driver ::"
-echo "checking available drivers... ( will auto install recomended )"
-ubuntu-drivers devices
-sudo ubuntu-drivers autoinstall
-echo "drivers installed. currently installed drivers:"
-ubuntu-drivers list
+echo "install graphics driver? y/n"
+read readInstallGraphicsDriver
+if [ "$readInstallGraphicsDriver" == "y" ]; then
+	#graphic driver
+	echo ":: graphics driver ::"
+	echo "checking available drivers... ( will auto install recomended )"
+	ubuntu-drivers devices
+	sudo ubuntu-drivers autoinstall
+	echo "drivers installed. currently installed drivers:"
+	ubuntu-drivers list
+	echo ""
+fi
 
-# KCB
-echo ":: corsair keyboard colors ::"
-sudo apt-get install git build-essential cmake libudev-dev qt5-default zlib1g-dev libappindicator-dev libpulse-dev libquazip5-dev
-mkdir ~/kcb
-cd ~/kcb
-git clone https://github.com/ckb-next/ckb-next.git
-cd ckb-next
-./quickinstall
-cd ./build
-sudo make install
-systemctl enable ckb-daemon && systemctl start ckb-daemon
+echo "install KCB Keyboard driver? y/n"
+read readKCB
+if [ "$readKCB" == "y" ]; then
+	# KCB
+	echo ":: corsair keyboard colors ::"
+	sudo apt-get install git build-essential cmake libudev-dev qt5-default zlib1g-dev libappindicator-dev libpulse-dev libquazip5-dev
+	mkdir ~/kcb
+	cd ~/kcb
+	git clone https://github.com/ckb-next/ckb-next.git
+	cd ckb-next
+	./quickinstall
+	cd ./build
+	sudo make install
+	systemctl enable ckb-daemon && systemctl start ckb-daemon
+	echo ""
+fi
 
-# xmacro
-echo ":: xmacro ::"
-cd ~/
-git clone https://github.com/PrimeVest/xmacro.git
-mv ./xmacro ./.xmacro
-cd ./.xmacro/scripts
-./setup.sh
+echo "Install XMacro? y/n"
+read readXMacro
+if [ "$readXMacro" == "y" ]; then
+	# xmacro
+	echo ":: xmacro ::"
+	cd ~/
+	git clone https://github.com/PrimeVest/xmacro.git
+	mv ./xmacro ./.xmacro
+	cd ./.xmacro/scripts
+	./setup.sh
+	echo ""
+fi
 
-# mkt_server
-echo ":: golang mkt server ::"
-mkdir -p ~/go/src
-cd ~/go/src
-echo "usr: Pr..Ve"
-echo "pass: ww2 rocket paper"
-git clone https://xp-dev.com/git/game_mkt_server
-mv game_mkt_server mkt
-cd ./mkt
-./commands.sh s
+echo "Install golang mkt server? y/n"
+read readMktServer
+if [ "$readMktServer" == "y" ]; then
+	# mkt_server
+	echo ":: golang mkt server ::"
+	mkdir -p ~/go/src
+	cd ~/go/src
+	echo "usr: Pr..Ve"
+	echo "pass: ww2 rocket paper"
+	git clone https://xp-dev.com/git/game_mkt_server
+	mv game_mkt_server mkt
+	cd ./mkt
+	./commands.sh s
+	echo ""
+fi
 
 
 
