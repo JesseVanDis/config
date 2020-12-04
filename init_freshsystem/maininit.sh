6911bb23b9964a626b59f9205ffbc41c79a10c2d
Jesse van Dis <pri_jesse@hotmail.com>
2020-12-04 10:30:03 +0100

    already installed stuff will now be skipped

--- init_freshsystem/maininit.sh
+++ init_freshsystem/maininit.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+cd "$(dirname "$0")"
 
 # todo:
 # - install chrome ( google-chrome-stable )
@@ -6,50 +8,67 @@
 # - buzzing audio: https://www.reddit.com/r/archlinux/comments/7vz8k6/since_recent_update_linux_puts_sound_to_sleep_on/
 #      ( read last part )
 
+Disabled=./maininit.sh
+stateDir="./.state/"
 
 
-checkPreventStandby=0
-checkGraphicsDriver=1
-checkKCB=1
-checkXMacro=0
-checkMKTServer=0
-checkGit=1
+checkPreventStandby="${stateDir}checkPreventStandby"
+checkGraphicsDriver="${stateDir}checkGraphicsDriver"
+checkCuda_10_1="${stateDir}checkCuda_10_1"
+checkKCB="${stateDir}checkKCB"
+checkXMacro="${stateDir}checkXMacro"
+checkMKTServer="${stateDir}checkMKTServer"
+checkGit="${stateDir}checkGit"
+
+
+# disabled:
+checkPreventStandby="$Disabled"
+checkXMacro="$Disabled"
+checkMKTServer="$Disabled"
+
 
 # ----------------
 # CHECK
 # ----------------
 
-if [ $checkPreventStandby == 1 ]; then
+
+if [ ! -f "${checkPreventStandby}" ]; then
 	echo "Prevent system going to standby? y/n"
 	read readPreventStandby
 	echo ""
 fi
 
-if [ $checkGraphicsDriver == 1 ]; then
+if [ ! -f "${checkGraphicsDriver}" ]; then
 	echo "install graphics driver? y/n"
 	read readInstallGraphicsDriver
 	echo ""
 fi
 
-if [ $checkKCB == 1 ]; then
+if [ ! -f "${checkCuda_10_1}" ]; then
+	echo "install cuda 10.1 ? ( will also switch gcc to version 8 ) y/n"
+	read readInstallCuda_10_1
+	echo ""
+fi
+
+if [ ! -f "${checkKCB}" ]; then
 	echo "install KCB Keyboard driver? y/n"
 	read readKCB
 	echo ""
 fi
 
-if [ $checkXMacro == 1 ]; then
+if [ ! -f "${checkXMacro}" ]; then
 	echo "Install XMacro? y/n"
 	read readXMacro
 	echo ""
 fi
 
-if [ $checkMKTServer == 1 ]; then
+if [ ! -f "${checkMKTServer}" ]; then
 	echo "Install golang mkt server? y/n"
 	read readMktServer
 	echo ""
 fi
 
-if [ $checkGit == 1 ]; then
+if [ ! -f "${checkGit}" ]; then
 	echo "setup git? y/n"
 	read readSetupGit
 	echo ""
@@ -58,12 +77,14 @@ fi
 # ----------------
 # INSTALL
 # ----------------
+mkdir -p "${stateDir}"
 
 if [ "$readPreventStandby" == "y" ]; then
 	#Prevent standby
 	echo ":: prevent from going standby ::"
 	sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
 	echo ""
+	echo "1" > "${checkPreventStandby}"
 fi
 
 
@@ -76,7 +97,19 @@ if [ "$readInstallGraphicsDriver" == "y" ]; then
 	echo "drivers installed. currently installed drivers:"
 	ubuntu-drivers list
 	echo ""
+	echo "1" > "${checkGraphicsDriver}"
 fi
+
+if [ "$readInstallCuda_10_1" == "y" ]; then
+	#cuda 10.1
+	echo ":: Cuda 10.1 ::"
+	sudo apt -y install nvidia-cuda-toolkit build-essential gcc-8 g++-8
+	nvcc --version
+	sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 8
+	sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-8 8
+	echo "1" > "${checkCuda_10_1}"
+fi
+
 
 if [ "$readKCB" == "y" ]; then
 	# KCB
@@ -92,6 +125,7 @@ if [ "$readKCB" == "y" ]; then
 	sudo make install
 	systemctl enable ckb-daemon && systemctl start ckb-daemon
 	echo ""
+	echo "1" > "${checkKCB}"
 fi
 
 if [ "$readXMacro" == "y" ]; then
@@ -103,6 +137,7 @@ if [ "$readXMacro" == "y" ]; then
 	cd ./.xmacro/scripts
 	./setup.sh
 	echo ""
+	echo "1" > "${checkXMacro}"
 fi
 
 if [ "$readMktServer" == "y" ]; then
@@ -116,14 +151,17 @@ if [ "$readMktServer" == "y" ]; then
 	cd ./mkt
 	./commands.sh s
 	echo ""
+	echo "1" > "${checkMKTServer}"
 fi
 
 if [ "$readSetupGit" == "y" ]; then
 	# setup_git
+	echo ":: Git ::"
 	sudo apt-get -y install git
 	git config --global credential.helper store # save username and password when pulling
 	git config --global user.email "pri_jesse@hotmail.com"
 	git config --global user.name "Jesse van Dis"
+	echo "1" > "${checkGit}"
 fi
 
 
